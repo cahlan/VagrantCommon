a47f32c5f069f636b0d0466c1aaceaf831377d30
Jay Marcyes <github@marcyes.com>
2012-03-02 16:02:48 -0800

    adds mbak function to bash that doesn't create a copy but renames the file

--- cookbooks/bash/files/default/bash_aliases.sh
+++ cookbooks/bash/files/default/bash_aliases.sh
@@ -80,9 +80,13 @@ function whereg(){
 }
 
 # http://stackoverflow.com/a/68600/5006
-#? bak <filepath> -> rename a file from filepath to filepath.bak
+#? bak <filepath> -> make a copy of filepath named filepath.bak
 function bak(){
   cp $1{,.bak}
+}
+#? mbak <filepath> -> rename a file from filepath to filepath.bak
+function mbak(){
+  mv $1{,.bak}
 }
 
 #? ret -> returns the result code of the last run command
