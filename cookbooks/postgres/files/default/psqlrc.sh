1af761e95c531dce92c829ff3d990c91b459be8b
Jay Marcyes <github@marcyes.com>
2012-03-29 00:22:02 -0700

    adds a default psqlrc to postgres

---
+++ cookbooks/postgres/files/default/psqlrc.sh
@@ -0,0 +1,8 @@
+\set QUIET
+\encoding unicode
+\set HISTFILE ~/.psql_history- :DBNAME
+\pset null 'NULL'
+\timing
+\pset pager off
+\x
+\unset QUIET
--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -87,6 +87,24 @@ case node[:platform]
     
     end
     
+    # add the .psqlrc file to all the users if it doesn't already exist
+    users_home = Dir.glob("/home/*/")
+    users_home << "/root/"
+    users_home.each do |user_home|
+    
+      user = File.basename(user_home)
+      
+      # http://wiki.opscode.com/display/chef/Resources#Resources-CookbookFile
+      cookbook_file File.join(user_home,".psqlrc") do
+        backup false
+        source "psqlrc.sh"
+        owner user
+        group user
+        mode "0644"
+      end
+    
+    end
+    
     # http://wiki.opscode.com/display/chef/Resources#Resources-Service
     service "postgres" do
       service_name "postgresql"
