cf6b3be444f80d2a11ab566ff5b01b1035474193
Jay Marcyes <github@marcyes.com>
2012-03-30 13:31:37 -0700

    updates the uwsgi init.d script to be more bulletproof since it was sometimes failing to stop the process before it tried to start them again which would cause the init.d to get unsynced and making it so you would have to force kill uwsgi

--- cookbooks/uwsgi/files/default/uwsgi.sh
+++ cookbooks/uwsgi/files/default/uwsgi.sh
@@ -13,6 +13,37 @@ test -f $DAEMON || exit 0
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# returns whether the passed in pid file is still running
+# you can use this with -n and -z of test, it will echo the pid if it is still running, or
+# be an empty length string if it isn't running 
+is_running(){
+
+  if [ -f "$1" ]; then
+  
+    pid=$(cat $1)
+    ps -eo pid | grep $pid
+    
+  fi
+  
+}
+
+# kill the running processes tied to the passed in pid file, wait until it is killed
+do_stop(){
+
+  if [ -n "$(is_running $1)" ]; then
+  
+    pid=$(cat $1)
+    $DAEMON --stop $1
+  
+    until [ -z "$(is_running $1)" ]; do
+      sleep 1
+    done
+  
+  fi
+
+}
+
+# start up a uwsgi server instance for each conf file found 
 for f in $(ls $APPS_DIR/*); do
   if [ -f $f ]; then
   
@@ -25,7 +56,6 @@ for f in $(ls $APPS_DIR/*); do
     pid="/var/run/$filename.pid"
     
     start_cmd="$DAEMON --$ext $f --daemonize $log --pidfile $pid"
-    stop_cmd="$DAEMON --stop $pid"
     reload_cmd="$DAEMON --reload $pid"
 
     case "$1" in
@@ -39,15 +69,13 @@ for f in $(ls $APPS_DIR/*); do
       
       stop)
       echo -n "Stopping $NAME - $filename: "
-      if [ -f "$pid" ]; then
-        $stop_cmd
-      fi
+      do_stop $pid
       echo "done."
       ;;
       
       reload|force-reload)
       echo -n "Reloading $NAME - $filename: "
-      if [ -f "$pid" ]; then
+      if [ -n "$(is_running $1)" ]; then
         $reload_cmd
       else
         $start_cmd
@@ -56,11 +84,8 @@ for f in $(ls $APPS_DIR/*); do
       ;;
       
       restart)
-      if [ -f "$pid" ]; then
-        echo -n "Restarting $NAME - $filename: "
-        $stop_cmd
-        sleep 1
-      fi
+      echo -n "Restarting $NAME - $filename: "
+      do_stop $pid
       $start_cmd
       echo "done."
       ;;
